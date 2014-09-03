e7b2073b2572f0701ff0c970d560c1dcc89f227b
Andreas <urban.andreas@gmail.com>
2014-09-03 21:30:37 +0200

    Logic for decide lights on off dependent of time/date of year.

--- src/tell/logger/lights/time/LightsTimeDescision.java
+++ src/tell/logger/lights/time/LightsTimeDescision.java
@@ -1,18 +1,41 @@
 package tell.logger.lights.time;
 
+import org.joda.time.DateTime;
+import org.joda.time.Interval;
+import org.joda.time.LocalTime;
+import org.joda.time.Minutes;
+
 /**
  * Calculate if lights should be turned on by look at time of day (depending on
- * date of the year) Optional should let calling class make other descision
- * (like weather integration)
+ * date of the year)
  */
-public class LightsTimeDescision {
+public class LightsTimeDescision implements LightsDecision {
 
-	public enum Decision {
-		ON, OPTIONAL, OFF
+	public Decision decide(DateTime now) {
+		String startMonth = now.monthOfYear().getAsText().toUpperCase();
+		String nextMonth = now.plusMonths(1).monthOfYear().getAsText().toUpperCase();
+
+		Interval dawn = calcPeriod(now, Dawn.valueOf(startMonth).getStartLights(), Dawn.valueOf(startMonth).getStopLights(), Dawn.valueOf(nextMonth).getStartLights(), Dawn.valueOf(nextMonth)
+				.getStopLights());
+		Interval dusk = calcPeriod(now, Dusk.valueOf(startMonth).getStartLights(), Dusk.valueOf(startMonth).getStopLights(), Dusk.valueOf(nextMonth).getStartLights(), Dusk.valueOf(nextMonth)
+				.getStopLights());
+
+		if (dawn.contains(now) || dusk.contains(now)) {
+			return Decision.ON;
+		} else {
+			return Decision.OFF;
+		}
+
 	}
 
-	public Decision check() {
-		return Decision.OFF;
+	private Interval calcPeriod(DateTime now, LocalTime start, LocalTime end, LocalTime startNextMonth, LocalTime endNextMonth) {
+		double minutesPerDayStart = (double) Minutes.minutesBetween(start, startNextMonth).getMinutes() / now.dayOfMonth().getMaximumValue();
+		double minutesPerDayEnd = (double) Minutes.minutesBetween(end, endNextMonth).getMinutes() / now.dayOfMonth().getMaximumValue();
+
+		start = start.plusMinutes((int) (minutesPerDayStart * now.dayOfMonth().get()));
+		end = end.plusMinutes((int) (minutesPerDayEnd * now.dayOfMonth().get()));
+
+		return new Interval(now.withFields(start), now.withFields(end));
 	}
 
 }
---
+++ test/tell/logger/lights/time/LightsTimeDecisionTest.java
@@ -0,0 +1,32 @@
+package tell.logger.lights.time;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+
+public class LightsTimeDecisionTest {
+
+	LightsDecision lightsDecision = new LightsTimeDescision();
+
+	@Test
+	public void testStartOfMonth() throws Exception {
+		assertThat(lightsDecision.decide(DateTime.parse("2014-09-03T18:30:05Z")), is(LightsDecision.Decision.OFF));
+	}
+
+	@Test
+	public void testEndOfMonth() throws Exception {
+		assertThat(lightsDecision.decide(DateTime.parse("2014-09-27T18:30:05Z")), is(LightsDecision.Decision.ON));
+	}
+
+	@Test
+	public void testStartOfMonthDusk() throws Exception {
+		assertThat(lightsDecision.decide(DateTime.parse("2014-12-05T09:10:05Z")), is(LightsDecision.Decision.OFF));
+	}
+
+	@Test
+	public void testEndOfMonthDusk() throws Exception {
+		assertThat(lightsDecision.decide(DateTime.parse("2014-12-30T09:10:05Z")), is(LightsDecision.Decision.ON));
+	}
+}
